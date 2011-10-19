a7136e8d8330629668c6eb5b54e73660f111db6a
Daniel Larimer <dlarimer@gmail.com>
2011-10-19 11:12:07 -0400

    made methods inline to fix linking errors

--- include/boost/cmt/signals.hpp
+++ include/boost/cmt/signals.hpp
@@ -1,17 +1,18 @@
 #ifndef _BOOST_CMT_SIGNALS_HPP
 #define _BOOST_CMT_SIGNALS_HPP
 #include <boost/signal.hpp>
+#include <boost/bind.hpp>
 #include <boost/cmt/task.hpp>
 
 namespace boost { namespace cmt {
    template<typename T>
-   T wait( boost::signal<void(T)>& sig, const microseconds& timeout_us=microseconds::max() ) {
+   inline T wait( boost::signal<void(T)>& sig, const microseconds& timeout_us=microseconds::max() ) {
        typename promise<T>::ptr p(new promise<T>());
        boost::signals::scoped_connection c = sig.connect( boost::bind(&promise<T>::set_value,p,_1) );
        return p->wait( timeout_us ); 
    }
 
-   void wait( boost::signal<void()>& sig, const microseconds& timeout_us=microseconds::max() ) {
+   inline void wait( boost::signal<void()>& sig, const microseconds& timeout_us=microseconds::max() ) {
        promise<void_t>::ptr p(new promise<void_t>());
        boost::signals::scoped_connection c = sig.connect( boost::bind(&promise<void_t>::set_value,p,void_t()) );
        p->wait( timeout_us ); 
